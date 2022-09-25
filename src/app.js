fc22a9bd23e3d25b807fae27817c7733ac2f7c3b
Vinicius <viniciuspedrosoreis@gmail.com>
2022-09-25 17:02:14 -0300

    keyboard toogle & funcition  ideias

--- src/app.js
+++ src/app.js
@@ -3,47 +3,86 @@ import styled from 'styled-components';
 import palavras from './palavras';
 import { useState } from 'react';
 
+{/* 
+let RightWord = []
+let SolvedWord = []
+*/}  
+
+
 export default function App() {
+    const [startgame, setStartGame] = useState(false)
     const [erros, setErros] = useState(0)
-    const [start, setStart] = useState(false)
-
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
+    let palavra = ""
     return (
         <Container>
             <Header>
-                <img src="img/forca0.png" />
+                {erros === 0 && <img src="img/forca0.png" /> }
+                {erros === 1 && <img src="img/forca1.png" /> }
+                {erros === 2 && <img src="img/forca2.png" /> }
+                {erros === 3 && <img src="img/forca3.png" /> }
+                {erros === 4 && <img src="img/forca4.png" /> }
+                {erros === 5 && <img src="img/forca5.png" /> }
+                {erros >= 6 && <img src="img/forca6.png" /> }
                 <LayoutButton>
-                    <Escolher onClick={() => StartGame()}>
+                    <Escolher onClick={() => palavra =  ComecoGame({setErros, setStartGame})}>
                         <p>Escolher Palavra</p>
                     </Escolher>
                 </LayoutButton>
                 <GameWord>
-                    <p>________</p>
+                    {startgame && <p>{palavra}</p> }
                 </GameWord>
             </Header>
             <AlfabetoLine>
-                {alfabeto.map((s) => <LetterButton><p>{s}</p></LetterButton> )}
+                {(startgame === false || erros >= 6) && alfabeto.map((s) => 
+                    <LetterOff><p>{s}</p>
+                    </LetterOff> )}
+                {startgame === true && erros < 6 && alfabeto.map((s) => 
+                    <LetterOn ><p>{s}</p></LetterOn> )}
             </AlfabetoLine>
 
             <GuessBox>
                 <p>Já sei a palavra!</p>
                 <GuessImput></GuessImput>
-                <GuessButton><p>Chutar</p></GuessButton>
+                <GuessButton onClick={() => setErros(erros + 1)}><p>Chutar</p></GuessButton>
             </GuessBox>
         </Container>
 
     );
+}
+{/* 
+    vaule ={s} onClick={ 
+    e => TryLetter(e.target.value, ActualWord)}
+*/}  
 
+function ComecoGame({setErros, setStartGame}){
+    setErros(0)
+    setStartGame(true)
+    const min = 0;
+    const max = 231;
+    const rand = Math.round(min + Math.random() * (max));
+    const palavraForca = palavras[rand]
+    console.log(palavraForca)
+    return (palavraForca)
 }
 
-function StartGame(){
-    setStart(true)
+{/*
+function TryLetter (Letter, palavra){
+    let wordsize = palavra.length
+    for (i = 0; i < wordsize; i++)(
+        if (Letter === palavra[i]){
+
+        }
+    )
+
 }
+*/}
+
 
 const LayoutButton = styled.div`
     padding-top: 47px;
     display: flex;
+    justify-content: end;
 `
 
 const Escolher = styled.button`
@@ -67,10 +106,10 @@ const AlfabetoLine =styled.div`
     display: flex;
     padding-left: 10%;
     padding-right: 10%;
-    justify-content: space-evenly;
+    justify-content: space-between;
 `
 
-const LetterButton = styled.button`
+const LetterOff = styled.button`
     width: 40px;
     height: 40px;
     background-color: gray;
@@ -81,6 +120,21 @@ const LetterButton = styled.button`
     justify-content: center;
     p{
         color: darkgrey;
+    }
+`
+
+const LetterOn = styled.button`
+    width: 40px;
+    height: 40px;
+    background-color: lightblue;
+    border-radius: 4px;
+    border-width: 1px;
+    border-color: darkblue;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    p{
+        color: darkblue;
     }
 `
 
