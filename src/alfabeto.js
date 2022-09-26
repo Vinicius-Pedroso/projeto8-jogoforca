1cd26597cb04bd9ef237ee324ce6003ab58521d2
Vinicius <viniciuspedrosoreis@gmail.com>
2022-09-25 21:35:22 -0300

    better layout, word showing

---
+++ src/alfabeto.js
@@ -0,0 +1,110 @@
+const alfabeto = [
+    {
+        letra: "a",
+        estado: false
+      },
+      {
+        letra: "b",
+        estado: false
+      },
+      {
+        letra: "c",
+        estado: false
+      },
+      {
+        letra: "d",
+        estado: false
+      },
+      {
+        letra: "e",
+        estado: false
+      },
+      {
+        letra: "f",
+        estado: false
+      },
+      {
+        letra: "g",
+        estado: false
+      },
+      {
+        letra: "h",
+        estado: false
+      },
+      {
+        letra: "i",
+        estado: false
+      },
+      {
+        letra: "j",
+        estado: false
+      },
+      {
+        letra: "k",
+        estado: false
+      },
+      {
+        letra: "l",
+        estado: false
+      },
+      {
+        letra: "m",
+        estado: false
+      },
+      {
+        letra: "n",
+        estado: false
+      },
+      {
+        letra: "o",
+        estado: false
+      },
+      {
+        letra: "p",
+        estado: false
+      },
+      {
+        letra: "q",
+        estado: false
+      },
+      {
+        letra: "r",
+        estado: false
+      },
+      {
+        letra: "s",
+        estado: false
+      },
+      {
+        letra: "t",
+        estado: false
+      },
+      {
+        letra: "u",
+        estado: false
+      },
+      {
+        letra: "v",
+        estado: false
+      },
+      {
+        letra: "w",
+        estado: false
+      },
+      {
+        letra: "x",
+        estado: false
+      },
+      {
+        letra: "y",
+        estado: false
+      },
+      {
+        letra: "z",
+        estado: false
+      },
+]
+
+export default alfabeto;
+
+//const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
--- src/app.js
+++ src/app.js
@@ -3,80 +3,78 @@ import styled from 'styled-components';
 import palavras from './palavras';
 import { useState } from 'react';
 
-{/* 
-let RightWord = []
-let SolvedWord = []
-*/}  
-
-
 export default function App() {
-    const [startgame, setStartGame] = useState(false)
-    const [erros, setErros] = useState(0)
+    const [gameGoing, setGameGoing] = useState(false)
+    const [error, setError] = useState(0)
+    const [rightWord, setRightWord] = useState("")
+    const [currentWord, setCurrentWord] = useState("")
+    const [lettersDone, setLettersDone] = ("")
+
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    let palavra = ""
+
     return (
         <Container>
             <Header>
-                {erros === 0 && <img src="img/forca0.png" /> }
-                {erros === 1 && <img src="img/forca1.png" /> }
-                {erros === 2 && <img src="img/forca2.png" /> }
-                {erros === 3 && <img src="img/forca3.png" /> }
-                {erros === 4 && <img src="img/forca4.png" /> }
-                {erros === 5 && <img src="img/forca5.png" /> }
-                {erros >= 6 && <img src="img/forca6.png" /> }
-                <LayoutButton>
-                    <Escolher onClick={() => palavra =  ComecoGame({setErros, setStartGame})}>
-                        <p>Escolher Palavra</p>
-                    </Escolher>
-                </LayoutButton>
-                <GameWord>
-                    {startgame && <p>{palavra}</p> }
-                </GameWord>
+                {error === 0 && <img src="img/forca0.png" />}
+                {error === 1 && <img src="img/forca1.png" />}
+                {error === 2 && <img src="img/forca2.png" />}
+                {error === 3 && <img src="img/forca3.png" />}
+                {error === 4 && <img src="img/forca4.png" />}
+                {error === 5 && <img src="img/forca5.png" />}
+                {error >= 6 && <img src="img/forca6.png" />}
+                <LeftColunm>
+                    <LayoutButton>
+                        <Escolher onClick={() => StartOfTheGame({ setError, setGameGoing, setRightWord, setCurrentWord })}>
+                            <p>Escolher Palavra</p>
+                        </Escolher>
+                    </LayoutButton>
+                    <GameWord>
+                        {gameGoing && (currentWord !== rightWord) && error < 6 && <h1>{currentWord}</h1>}
+                        {error >= 6 && <GameLost><h1>{rightWord}</h1></GameLost>}
+                        {(currentWord === rightWord) && <GameWon><h1>{rightWord}</h1></GameWon>}
+                    </GameWord>
+                </LeftColunm>
+
             </Header>
             <AlfabetoLine>
-                {(startgame === false || erros >= 6) && alfabeto.map((s) => 
+                {(gameGoing === false || error >= 6) && alfabeto.map((s) =>
                     <LetterOff><p>{s}</p>
-                    </LetterOff> )}
-                {startgame === true && erros < 6 && alfabeto.map((s) => 
-                    <LetterOn ><p>{s}</p></LetterOn> )}
+                    </LetterOff>)}
+                {gameGoing === true && error < 6 && alfabeto.map((s) =>
+                    <LetterOn onClick={() => TryLetter({ s, setLettersDone })}><p>{s}</p></LetterOn>)}
             </AlfabetoLine>
 
             <GuessBox>
                 <p>Já sei a palavra!</p>
                 <GuessImput></GuessImput>
-                <GuessButton onClick={() => setErros(erros + 1)}><p>Chutar</p></GuessButton>
+                <GuessButton onClick={() => setError(error + 1)}><p>Chutar</p></GuessButton>
             </GuessBox>
         </Container>
 
     );
 }
-{/* 
-    vaule ={s} onClick={ 
-    e => TryLetter(e.target.value, ActualWord)}
-*/}  
 
-function ComecoGame({setErros, setStartGame}){
-    setErros(0)
-    setStartGame(true)
+function TryLetter({ Letter, setLettersDone }) {
+    console.log(Letter)
+
+    setLettersDone(+ Letter);
+
+}
+
+function StartOfTheGame({ setError, setGameGoing, setRightWord, setCurrentWord }) {
+    setError(0)
+    setGameGoing(true)
     const min = 0;
     const max = 231;
     const rand = Math.round(min + Math.random() * (max));
-    const palavraForca = palavras[rand]
-    console.log(palavraForca)
-    return (palavraForca)
+    setRightWord(palavras[rand])
+    let current = []
+    for (let i = 0; i < palavras[rand].length; i++) {
+        current.push("_ ")
+    }
+    setCurrentWord(current)
+    return;
 }
-
-{/*
-function TryLetter (Letter, palavra){
-    let wordsize = palavra.length
-    for (i = 0; i < wordsize; i++)(
-        if (Letter === palavra[i]){
-
-        }
-    )
-
-}
-*/}
 
 
 const LayoutButton = styled.div`
@@ -88,19 +86,35 @@ const LayoutButton = styled.div`
 const Escolher = styled.button`
     background-color: green;
     border-radius: 5px;
-    width: 120px;
+    width: 137px;
     height: 40px;
     p{
+        font-size: 12px;
         color: white;
     }
+`
+
+const LeftColunm = styled.div`
+    padding-left: 105px;
+    display: block;
 `
 
 const Header = styled.div`
     display: flex;
     padding-bottom: 30px;
 `
-
-const AlfabetoLine =styled.div`
+const GameWord = styled.div`
+    margin-top: 550px;
+    display: flex;
+    align-items: end;
+    justify-content: end;
+    p{
+        display: flex;
+        margin-left: 3px;
+        color: darkgray;
+    }
+`
+const AlfabetoLine = styled.div`
     width: 100%;
     padding-bottom: 10px;
     display: flex;
@@ -138,6 +152,18 @@ const LetterOn = styled.button`
     }
 `
 
+const GameLost = styled.div`
+    h1{
+        color: red;
+    }
+`
+
+const GameWon = styled.div`
+    h1{
+        color: green;
+    }
+`
+
 const Container = styled.div`
     padding-top: 20px;
     padding-left: 20px;
@@ -147,6 +173,16 @@ const Container = styled.div`
         max-width: 80%;
         max-height: 70%;
     }
+
+    p {
+        font-weight: bold;
+    }
+
+    h1 {
+        font-size: 36px;
+        font-weight: bold;
+        color: black;
+    }
 `
 
 const GuessBox = styled.div`
@@ -155,13 +191,15 @@ const GuessBox = styled.div`
     align-items: center;
 `
 const GuessImput = styled.input`
+height: 30px;
     border-radius: 3px;
     border-width: 1px;
     border-color: black;
 `
 const GuessButton = styled.button`
-height: 20px;
-width: 50px;
+margin-left: 20px;
+height: 30px;
+width: 100px;
     background-color: lightblue;
     border-radius: 4px;
     border-width: 2px;
@@ -170,13 +208,4 @@ width: 50px;
         color: blue;
     }
 
-`
-const GameWord = styled.div`
-    display: flex;
-    align-items: end;
-    justify-content: end;
-    p{
-        color: darkgray;
-        font-size: 20px;
-    }
 `
